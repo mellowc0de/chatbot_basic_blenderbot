3daa34c3c2fb4da7617b01395f55b4f3b6c29dd4
mellowc0de <mellohcode@gmail.com>
2025-11-30 13:04:07 -0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,25 +1,41 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import json
+from flask import Flask, request
+from flask_cors import CORS
+from transformers import AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer
+
+app = Flask(__name__)
+CORS(app)
 
 model_name = "facebook/blenderbot-400M-distill"
-
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-
 conversation_history = []
 
-while True:
-    history_string = "\n".join(conversation_history)
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-    input_text ="hello, how are you doing?"
+@app.route('/chatbot', methods=['POST'])
+def handle_prompt():
+    data = request.get_data(as_text=True)
+    data = json.loads(data)
+    input_text = data['prompt']
+
+    history_string = "\n".join(conversation_history)
 
     inputs = tokenizer.encode_plus(history_string, input_text, return_tensors="pt")
 
     tokenizer.pretrained_vocab_files_map
 
-    outputs = model.generate(**inputs)
+    outputs = model.generate(**inputs, max_length= 60)
 
     response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
-    print(response)
 
     conversation_history.append(input_text)
     conversation_history.append(response)
+
+    return response
+
+if __name__ == '__main__':
+    app.run()
